4d2e5c8889b3f35d9564c17b1e6eac33d2ae9707
quedevel <kdh90817@naver.com>
2023-05-21 15:02:28 +0900

    feat: build 시 불필요한 Test 제거

--- module-api/src/test/java/dev/be/moduleapi/ModuleApiApplicationTests.java
+++ module-api/src/test/java/dev/be/moduleapi/ModuleApiApplicationTests.java
@@ -3,7 +3,6 @@ package dev.be.moduleapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ModuleApiApplicationTests {
 
     @Test
